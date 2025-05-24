88439443656deee49308f35b96df2d4d1d0ad013
GustavoZanardi15 <ulianzanardig@gmail.com>
2025-05-24 09:08:26 -0300

    feat: implementa login com geração de token JWT no endpoint /auth/login

--- src/main/java/com/example/gerenciador/controller/AuthController.java
+++ src/main/java/com/example/gerenciador/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.example.gerenciador.controller;
 
+import com.example.gerenciador.dto.LoginRequest;
 import com.example.gerenciador.model.UsuarioModel;
 import com.example.gerenciador.repository.UsuarioRepository;
+import com.example.gerenciador.security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,10 @@ public class AuthController {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
+
     @PostMapping("/register")
     public ResponseEntity<?> registrarUsuario(@RequestBody UsuarioModel usuario) {
         if (usuarioRepository.findByUsername(usuario.getUsername()).isPresent()) {
@@ -21,5 +27,27 @@ public class AuthController {
 
         UsuarioModel novoUsuario = usuarioRepository.save(usuario);
         return ResponseEntity.ok(novoUsuario);
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest login) {
+
+        return usuarioRepository.findByUsername(login.getUsername())
+                .map(usuario -> {
+
+                    if (usuario.getPassword().equals(login.getPassword())) {
+
+                        String token = jwtUtil.generateToken(usuario.getUsername());
+
+
+                        return ResponseEntity.ok("Bearer " + token);
+                    }
+
+
+                    return ResponseEntity.status(401).body("Senha inválida");
+                })
+
+                .orElse(ResponseEntity.status(401).body("Usuário não encontrado"));
     }
 }
---
+++ src/main/java/com/example/gerenciador/security/JwtUtil.java
@@ -0,0 +1,30 @@
+package com.example.gerenciador.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET = "senha-secreta-123";
+    private final long EXPIRATION_TIME = 86400000; // 1 dia
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
